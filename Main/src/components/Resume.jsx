f24786566a1bf0b278676c89202c94cf3e238c31
khinoj <kristopher.hinojosa@gmail.com>
2023-11-25 11:07:11 -0500

    Resume image and download link

--- Main/src/components/Resume.jsx
+++ Main/src/components/Resume.jsx
@@ -1,7 +1,14 @@
+
+// import resumeFile from "./public/resume/Entry_Resume.pdf"; 
+// import resumeImage from "./public/images/pdf-image.png"; 
+
 const Resume = () => {
     return (
-        <main>
-            <h1> hi </h1>
+        <main className="d-flex flex-column align-items-center">
+            <a href="./public/resume/Entry_Resume.pdf" download style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <img src="./public/images/pdf-image.png" alt="Resume" className="my-3" />
+                <div className="btn btn-primary">Download Resume</div>
+            </a>
         </main>
     );
 }
