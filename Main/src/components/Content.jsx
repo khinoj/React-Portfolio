e65421e32f8fe9078ba2f443270c881fde078b14
khinoj <kristopher.hinojosa@gmail.com>
2023-11-25 13:48:56 -0500

    fixed content text spelling error

--- Main/src/components/Content.jsx
+++ Main/src/components/Content.jsx
@@ -12,9 +12,9 @@ const Content = () => {
             <img src="./public/images/image1.jpg" style={imageStyle} alt="Kris Image" />
             <br></br>
             <p style={{fontSize: '22px', textAlign: 'center'}}>
-                Good Afternoon, My Name is Kris Hinojosa and I'm a software developer. <br></br>
-                I enjoy programming because there is always a challenge. I may be new to programming but I enjoy learning something new on a daily bases.<br></br>
-                I am a new father and having my son really pushed me towards developing a skill set.
+                My Name is Kris Hinojosa and I'm a software developer. <br></br>
+                I enjoy programming because there is always a challenge. <br></br>
+                I am a new father and having my son really pushed me towards developing a skill set that would allow me to support my little man.
             </p>
         </main>
     );
